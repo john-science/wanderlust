825644ea611d905c3123797bfd9ee06b08e1e5dd
John Stilley <thejollysin@gmail.com>
2017-04-19 10:52:11 -0700

    placeholder for map world

---
+++ js/map.js
@@ -0,0 +1,14 @@
+/** Creata a map from a data file.
+    The goal will be to make a map this is:
+      accessible in pieces (without loading the whole thing at once),
+      loadable from a data file (JSON or KML).
+    The map should also have (or allow) for a richness of detail:
+      terrain,
+      ground cover,
+      canopy cover,
+      soil type,
+      weather,
+      water bodies,
+      animal population
+ */
+ 
