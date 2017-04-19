2502c32ff5bb3f61203df2590d86cf793532d433
John Stilley <thejollysin@gmail.com>
2017-04-19 10:44:49 -0700

    placeholder

---
+++ js/intro.js
@@ -0,0 +1,5 @@
+/** Add a handler that accepts any keystroke or mouse action to wipe the screen,
+    removes the intro UI elements,
+    add the game UI elements,
+    and initializes the game.
+*/
