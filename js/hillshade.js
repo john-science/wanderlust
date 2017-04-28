e0463a585d8ede3c56bbcc8a7fe0d9cf52f5d198
John Stilley <thejollysin@gmail.com>
2017-04-28 11:13:57 -0700

    #4 now calculates whole grid at once

--- js/hillshade.js
+++ js/hillshade.js
@@ -1,21 +1,15 @@
-var el = [  /** Test elevation data */
-  [2450, 2461, 2483],
-  [2452, 2461, 2483],
-  [2447, 2455, 2477]
-];
-
-
 var HillShade = (function() {
   /** placeholder elevation grid variables */
   var el = [];
   var num_rows = 0;
   var num_cols = 0;
+
   /** TODO: Strange constants. Play with these. */
   var z_factor = 1.0;
   var cell_size = 5.0;
 
-  /** Testing input data. TODO: This will need to be configurable. */
-  var alt_rad = 0.7853981633974483;
+  /** Testing input data. */
+  var zen_rad = 0.7853981633974483;
   var azi_rad = 2.356194490192345;
 
   var calc_hillshade = function(Zenith_rad, Slope_rad, Azimuth_rad, Aspect_rad) {
@@ -34,21 +28,18 @@ var HillShade = (function() {
     return ((el[r + 1][c - 1] + 2 * el[r + 1][c] + el[r + 1][c + 1]) - (el[r - 1][c - 1] + 2 * el[r - 1][c] + el[r - 1][c + 1])) / (8 * cell_size);
   };
 
-  var calc_zenith_radians = function(altitude_rad) {
-    return 1.5707963267948966 - altitude_rad;
-  };
-
   // Slope_rad = ATAN ( z_factor * √ ( [dz/dx]2 + [dz/dy]2) ) 
   var calc_slope_rad = function(dz_dx, dz_dy) {
     return Math.atan(z_factor * Math.sqrt(dz_dx * dz_dx + dz_dy * dz_dy));
   };
 
   var calc_aspect_rad = function(dz_dx, dz_dy) {
-    var aspect_rad = 1.5707963267948966; // TODO: Better default?
+    var aspect_rad;
     if (dz_dx !== 0.0) {
       aspect_rad = Math.atan2(dz_dy, -dz_dx);
-    } else if (aspect_rad < 0) {
-      aspect_rad += 6.283185307179586;
+      if (aspect_rad < 0) {
+        aspect_rad += 6.283185307179586;
+      }
     } else if (dz_dx === 0.0) {
       if (dz_dy > 0) {
         apect_rad = 1.5707963267948966;
@@ -82,14 +73,46 @@ var HillShade = (function() {
       /** calculate the hill shade for one grid cell */
       var dz_dx = calc_slope_x(r, c);
       var dz_dy = calc_slope_y(r, c);
-      var zrad = calc_zenith_radians(alt_rad);
       var srad = calc_slope_rad(dz_dx, dz_dy);
       var asrad = calc_aspect_rad(dz_dx, dz_dy);
-      return calc_hillshade(zrad, srad, azi_rad, asrad);
-    }  /** TODO: Add a function to return an entire grid of hill shading */
+      return calc_hillshade(zen_rad, srad, azi_rad, asrad);
+    },
+    setZenith: function(rads) {
+      /** Zenith_degress = 90 - Altitude_degrees
+          Zenith_rad = Zenith * pi / 180.0
+       */
+      zen_rad = rads;
+    },
+    setAzimuth: function(rads) {
+      /** Azimuth_math = 360.0 - Azimuth_degrees + 90
+         Azimuth_math = Azimuth_math - 360.0
+         Azimuth_rad = Azimuth_math *  pi / 180.0
+       */
+      azi_rad = rads;
+    },
+    grid: function() {
+      /** return an entire grid of hillshade values */
+      var map = [];
+      for (var r = 0; r < num_rows; r++) {
+        var row = [];
+        for (var c = 0; c < num_cols; c++) {
+          row.push(calc(r, c));
+        }
+        map.push(row);
+      }
+      return map;
+    }
   };
 }());
 
 
-HillShade.init(el);
+/**
+// TODO: Testing
+var elevation = [
+  [2450, 2461, 2483],
+  [2452, 2461, 2483],
+  [2447, 2455, 2477]
+];
+HillShade.init(elevation);
 console.log(HillShade.calc(1, 1));
+*/
