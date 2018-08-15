8f72e63bc9c56034d287e3fa602aae6f512d23c2
John Stilley <thejollysin@gmail.com>
2018-08-15 09:10:52 -0400

    #22 connecting 8-hr sleep to "w" key

--- js/ui.js
+++ js/ui.js
@@ -22,7 +22,7 @@ document.getElementById("help").addEventListener("click", function(e) {
 var UI = {
   moveKeys: {36: [-1, -1], 38: [-1, 0], 33: [-1, 1], 37: [0, -1],
              39: [0, 1],   35: [1, -1], 40: [1, 0],  34: [1, 1]},  // row, col
-  waitKeys: {12: 30, 32: 30, 53: 30, 101: 30},
+  waitKeys: {12: 30, 32: 30, 53: 30, 101: 30, 87: 480},
 
   gameplay: function() {
     window.addEventListener('keydown', function(e) {
