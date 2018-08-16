ee708a07b28ccc172af754ca9696aec016bb7367
John Stilley <thejollysin@gmail.com>
2018-08-15 22:09:25 -0400

    #21 starting placeholder for weather modeling

---
+++ js/weather.js
@@ -0,0 +1,7 @@
+/** To start, we will only model the local temperature,
+    more detailed region-wide weather model will come later. */
+var Weather = {
+  temperature: function(elevation, hr, day of year) {
+    return 13;
+  }
+}
