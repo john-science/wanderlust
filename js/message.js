440e6e23b7f62c843e027ce07c3cb0f797d12036
John Stilley <thejollysin@gmail.com>
2018-09-06 16:48:57 -0400

    #26 filling outline

--- js/message.js
+++ js/message.js
@@ -1,14 +1,15 @@
 
 var Message = (function() {
 	var messages = ['You break camp and set out into the world...'];
-	var window_id = '0';
+	var window_id = 'message_log';
 
 	return {
 		add: function(mess) {this.messages.push(mess)},
+		clear: function() {this.messages = [];}  /** TODO: Also, wipe the text in the UI element */
 		get_all: function() {return this.messages},
 		print: function(mess) {
 			this.add(mess);
-			console.log(mess);
+			console.log(mess);  /** TODO: This is where the UI interaction will have to happen */
 		},
 	}
 })();
