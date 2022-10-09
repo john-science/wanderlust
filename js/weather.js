3e9084bafb71da29a0b4fa8abe3955c78e0bdc9d
John Stilley <1831479+john-science@users.noreply.github.com>
2022-10-09 07:49:14 -0700

    Testing if we can publish to GH Pages again

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,6 @@
 /** To start, we will only model the local temperature,
-more detailed region-wide weather model will come later. */
+more detailed region-wide weather model will come later.
+*/
 var Weather = (function() {
 	var hourly_temp = [0.27136, 0.16227, 0.077, 0.02173, 0.00021, 0.014, 0.0622, 0.212, 0.4219, 0.588, 0.7542, 0.85868, 0.9378, 0.98597, 0.99979, 0.97827, 0.92295, 0.83773, 0.72864, 0.60338, 0.47081, 0.34031, 0.2211, 0.1216];
 	var monthly_temp = [0.28125, 0.34375, 0.4375, 0.5625, 0.6875, 0.84375, 1.0, 1.0, 0.875, 0.6875, 0.40625, 0.25];
